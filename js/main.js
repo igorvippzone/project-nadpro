21525805eef66707d837848329245b3917b9ca21
Агаларов Игорь <igorvippzone@gmail.com>
2022-07-08 19:32:26 +0300

    first commitя

--- js/main.js
+++ js/main.js
@@ -1,25 +1,35 @@
 const body = document.querySelector("body");
 const buttonsOpenModal = document.querySelectorAll("[data-button]");
 
-function handlerToggle(button) {
-  button.removeEventListener("click", () => handlerToggle(button));
+function noScroll() {
+  if (body.classList.contains("noscroll")) {
+    body.style.paddingRight = 0;
+    body.classList.remove("noscroll");
+  } else {
+    const bodyWidth = body.offsetWidth;
+    const windowWidth = window.innerWidth;
+    const scrollWidth = windowWidth - bodyWidth;
+    body.classList.add("noscroll");
+    body.style.paddingRight = scrollWidth + "px";
+  }
+}
 
-  const bodyWidth = body.offsetWidth;
-  const windowWidth = window.innerWidth;
-  const scrollWidth = windowWidth - bodyWidth;
-  body.style.paddingRight = scrollWidth + "px";
-  body.classList.add("noscroll");
+
+
+
+function handlerToggle(button) {
+  noScroll();
 
   const getSearchModal = button.getAttribute("data-button");
   const modal = document.getElementById(getSearchModal);
   const buttonClose = modal.querySelector("[data-close]");
+  console.log(buttonClose);
   button.classList.add("active");
 
   const handleClose = () => {
     modal.classList.remove("active");
     button.classList.remove("active");
-    body.style.paddingRight = 0;
-    body.classList.remove("noscroll");
+    noScroll();
     buttonClose.removeEventListener("click", handleClose);
   };
 
@@ -29,5 +39,51 @@ function handlerToggle(button) {
 }
 
 buttonsOpenModal.forEach((button) => {
-  button.addEventListener("click", () => handlerToggle(button));
+  let title = "";
+  switch (button.getAttribute("data-button")) {
+    case "menu":
+      button.addEventListener("click", () => handlerToggle(button));
+      break;
+    case "city":
+      title = "Выберете город";
+      button.addEventListener("click", () => createModal(title));
+      break;
+
+    case "registration":
+      title = "Регистрация";
+      button.addEventListener("click", () => createModal(title));
+      break;
+
+    case "login":
+      title = "Вход";
+      button.addEventListener("click", () => createModal(title));
+      break;
+
+    default:
+      title = "Нет контента";
+      button.addEventListener("click", () => createModal(title));
+      break;
+  }
 });
+
+function createModal(title) {
+  const modalWrapper = document.createElement("div");
+  modalWrapper.classList.add("modal");
+
+  const modalContent = document.createElement("div");
+  modalContent.classList.add("content");
+
+  const buttonClose = document.createElement("button");
+  buttonClose.classList.add("close");
+
+  const mainTitle = document.createElement("h2");
+  mainTitle.textContent = title;
+
+  modalContent.prepend(mainTitle);
+  modalContent.append(buttonClose);
+
+  modalWrapper.prepend(modalContent);
+
+  body.prepend(modalWrapper);
+  noScroll()
+}
